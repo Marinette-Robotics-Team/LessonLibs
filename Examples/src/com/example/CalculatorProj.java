823c92fbb9144dbc4a6246c43628fed4cbe675da
bagoftea <34144650+bagoftea@users.noreply.github.com>
2017-12-03 13:13:48 -0600

    Added functionality to calc project
    The calculator project now works on user input to re-enforce the concepts of return types.

--- Examples/src/com/example/CalculatorProj.java
+++ Examples/src/com/example/CalculatorProj.java
@@ -1,10 +1,13 @@
 package com.example;
-
+import java.util.Scanner;
 public class CalculatorProj {
-
+static Scanner reader;
 	
 	//y'all are gonna make a calculator app
 	public static void main(String[] args) {
+		reader = new Scanner(System.in);
+		
+		
 		/*
 		 * The app will take 3 inputs:
 		 * the 2 numbers to act on, and the operator to act on them
@@ -20,11 +23,50 @@ public class CalculatorProj {
 		 * The result should be printed to the screen.
 		 * 
 		 */
-		double input1;  
-		double input2;
-		String operator;
+		
+		//To get user number input for the math problem
+		// use the method AskNumber();
+		// AskNumber(); returns a double
 		
 		
+		//To get user operator input
+		// use the method AskOperator();
+		// AskOperator(); returns a String
+		
+		
+		double Number1;  
+		double Number2;
+		String Operator;
+		
+		
+
+
+	}
+	
+	
+	//Dont worry about the fancy magic that goes on down here
+	//its me just abstracting stuff to make it easy
+	
+	
+	public static double AskNumber(){
+
+		//Lets the user type a number.
+		//Then Stores it in the Number Variable.
+		double Number = reader.nextDouble();
+
+		//This returns what is stored in the Number variable
+		return Number;
+
+	}
+	
+	public static String AskOperator(){
+
+		//Lets the user type in an Operator.
+		//Then Stores it in the Operator Variable.
+		String Operator = reader.next();
+
+		//This returns what is stored in the Operator variable
+		return Operator;
 
 	}
 
