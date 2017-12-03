2c7c68387cfc318629e3893fe6328985a19ec401
DriverStation <robotics@marinette.k12.wi.us>
2017-12-03 09:12:17 -0600

    Added project1 to an eclipse project for easier importing
    Also fixed some typos

---
+++ Project1/src/Project1.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class Project1{
+	static Scanner reader;
+
+
+	public static void main(String[] args){
+		reader = new Scanner(System.in);
+
+
+
+		String yourName;
+
+		double yourAge;
+
+
+  
+
+
+
+		// Here I want you ask for the users name
+		// and have the program store it as a variable.
+		// You could use System print statements to ask a question
+		// Ask name command example  AskName();
+		
+
+
+
+
+		// Here I want you to ask for the users age
+		// and have the program store it as a variable too.
+		// ask age command example  AskAge();
+
+
+
+
+
+
+		// Now I want you to greet the user by their name
+		// and say how old they are.
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+	// Don't Worry about this stuff
+
+
+
+	public static String AskName(){
+
+		//Lets the user type in their name.
+		//Then Stores it in the Name Variable.
+		String Name = reader.next();
+
+		//This returns what is stored in the Name variable
+		return Name;
+
+	}
+
+
+
+
+
+
+
+
+	public static double AskAge(){
+
+		//Lets the user type in their age.
+		//Then Stores it in the Age Variable.
+		double Age = reader.nextDouble();
+
+		//This returns what is stored in the Age variable
+		return Age;
+
+	}
+
+}
