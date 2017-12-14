71d86f811d693952990a53783d7901a153e93949
DriverStation <robotics@marinette.k12.wi.us>
2017-12-14 17:01:01 -0600

    Added Description to ControlSchemeLesson
    See title

--- ControlSchemeLesson/src/org/ControlSchemeLesson/ControlSchemeLesson.java
+++ ControlSchemeLesson/src/org/ControlSchemeLesson/ControlSchemeLesson.java
@@ -4,6 +4,25 @@ import org.usfirst.frc.team6624.robot.Robot;
 
 public class ControlSchemeLesson {
 	
+	/*
+	 * Your mission, should you choose to accept it, is to create a control scheme for the robot.
+	 * 
+	 * You should be able to maneuver the robot with 2 xbox joysticks.
+	 * You have access to to the following functions:
+	 * 
+	 * setLeftSpeed(double speed)
+	 * setRightSpeed(double speed) 
+	 * - These take a value between -1 and 1, set the speed of the left and right drivetrains
+	 * 
+	 * getLeftJoystickX()
+	 * getLeftJoystickY()
+	 * getRightJoystickX()
+	 * getRightJoystickY()
+	 * 
+	 * -These get the X and Y positions of the joysticks on the xbox controller, give values between -1 and 1
+	 * 
+	 */
+	
 	//runs once when the robot turns on
 	public void setup() {
 		
